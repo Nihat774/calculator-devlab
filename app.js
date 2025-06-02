cbc55382133fc419c831ff6e43dd8ec2ba92172f
Nihat774 <nihatmemmedov.0520@gmail.com>
2025-06-01 23:42:06 -1200

    added calculator

---
+++ app.js
@@ -0,0 +1,95 @@
+const btn = document.getElementsByClassName("btn");
+const display = document.querySelector(".display");
+const box = document.querySelector(".box");
+const previousData = document.querySelector(".previousData");
+const moon = document.querySelector(".moon");
+const sun = document.querySelector(".sun");
+let currentValue = "";
+let previousValue = "";
+let operandValue = "";
+
+function appendNumber(number) {
+  currentValue += number;
+  display.value = currentValue;
+}
+
+function appendOperand(operand) {
+  if (operandValue != "" && previousValue != "") {
+    previousData.value = previousValue;
+  }
+  if (currentValue == "") return;
+  if (previousValue != "") {
+    calculate();
+  }
+  operandValue = operand;
+  console.log(operandValue);
+  previousValue = currentValue;
+  currentValue = "";
+  display.value = `${previousValue} ${operandValue}`;
+}
+
+function calculate() {
+  if (previousValue == "" || currentValue == "") return;
+  let prev = parseFloat(previousValue);
+  let current = parseFloat(currentValue);
+  let result;
+  switch (operandValue) {
+    case "+":
+      result = prev + current;
+      break;
+    case "-":
+      result = prev - current;
+      break;
+    case "*":
+      result = prev * current;
+      break;
+    case "/":
+      if (current == "0") {
+        alert("0-a bölmə yoxdur.");
+      }
+      result = prev / current;
+      break;
+    case "%":
+      result = (prev * current) / 100;
+      break;
+    default:
+      break;
+  }
+  currentValue = result.toString();
+  operandValue = "";
+  previousValue = "";
+  display.value = currentValue;
+}
+
+function clearDisplay() {
+  currentValue = "";
+  display.value = currentValue;
+}
+
+window.onload = function () {
+  const mode = localStorage.getItem("mode") || "light";
+
+  if (mode === "dark") {
+    box.classList.add("dark-mode");
+    sun.classList.add("active");
+    moon.classList.remove("active");
+  } else {
+    box.classList.remove("dark-mode");
+    moon.classList.add("active");
+    sun.classList.remove("active");
+  }
+};
+
+function modeChange() {
+  if (box.classList.contains("dark-mode")) {
+    box.classList.remove("dark-mode");
+    localStorage.setItem("mode", "light");
+    moon.classList.add("active");
+    sun.classList.remove("active");
+  } else {
+    box.classList.add("dark-mode");
+    localStorage.setItem("mode", "dark");
+    sun.classList.add("active");
+    moon.classList.remove("active");
+  }
+}
